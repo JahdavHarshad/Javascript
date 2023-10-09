a6c9702cbb0ae55023d603ac862db71a6acdb925
JahdavHarshad <143888019+JahdavHarshad@users.noreply.github.com>
2023-10-09 13:01:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all js code as newer version.
+
+alert("hello");  // we are using node js not browser and alert is run only on browser.
+
+// number => 2 to power 53
+// bugint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefinded => 
+// symbol => unique
+
+
+//object
+
+// NULL is object but if you check undefind so it wll show type of undefined.
+
